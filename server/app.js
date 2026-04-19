f72c00103875e2046919c2d158d11b165f94150a
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-19 16:08:35 +0530

    added indes.ejs for products display

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const assets = require('./assest');
 const productroutes = require('./routes/productRoutes');
+const ejsMate = require('ejs-mate');
 
 // connect mongo db
 mongoose.connect('mongodb://127.0.0.1:27017/e-shop')
@@ -16,13 +17,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/e-shop')
 
 // views folder for static files like css and js
 app.use(express.static(path.join(__dirname, 'public')));
+
 // setting up view engine
+app.set('view engine', 'ejs'); // ejs is a templating language (read the views file)
+app.engine('ejs', ejsMate); // for using layout in ejs
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 
 // seed the database
-assets();
+// assets();
 
 // product routes - har incoming request to /products will be handled by productroutes
 app.use(productroutes);
