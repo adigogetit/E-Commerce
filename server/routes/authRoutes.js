defca953ac87992060bad011abfe779b28d02ff8
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-22 01:02:09 +0530

    added login and signup functionality in authroutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,18 +4,35 @@ const User = require('../models/userModel');
 const router = express.Router() //mini instance
 
 // to show the form of signup
-router.get('/register' , (req,res)=>{
+router.get('/register', (req, res) => {
     res.render('auth/signup');
 })
 
 // actually want to register a user in my DB
-router.post('/register' , async(req,res)=>{
+router.post('/register', async (req, res) => {
+    let { email, password, username, role } = req.body;
 
+    const user = new User({ email, username, role });
+
+    const newUser = await User.register(user, password);
+
+    res.redirect('/login');
 })
 
 // to get login form
-router.get('/login' , (req,res)=>{
+router.get('/login', (req, res) => {
     res.render('auth/login');
+})
+
+// to actually login via the db
+router.post('/login', 
+    passport.authenticate('local', { 
+        failureRedirect: '/login', 
+        failureMessage: true 
+    }),
+    (req,res)=>{
+        req.flash('success' , 'welcome back')
+        res.redirect('/products');
 })
 
 module.exports = router;
