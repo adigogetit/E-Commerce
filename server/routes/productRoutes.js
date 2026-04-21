87b2ae542f75458cf7548596a3e5a9e835fe6204
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-21 19:03:28 +0530

    added schema for server side validation

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,62 +3,88 @@ const Product = require('../models/productModel');
 const router = express.Router() //mini instance
 
 // to show all the products
-router.get('/products' , async(req,res)=>{
-    let products = await Product.find({});
-    res.render('products/index' , {products});
+router.get('/products', async (req, res) => {
+    try {
+        let products = await Product.find({});
+        res.render('products/index', { products });
+    } catch (err) {
+        res.status(500).render('error', { err: e.message });
+    }
 })
 
 // to show the form for new product
-router.get('/product/new' , (req,res)=>{
-    res.render('products/new');
+router.get('/product/new', (req, res) => {
+    try {
+        res.render('products/new');
+    } catch (err) {
+        res.status(500).render('error', { err: e.message });
+    }
 })
 
 // to add new product
-router.post('/products' ,async(req,res)=>{
-    let {name , img , price , desc} = req.body;
-    await Product.create({name , img , price , desc})
-    res.redirect('/products')
+router.post('/products', async (req, res) => {
+    try {
+        let { name, img, price, desc } = req.body;
+        await Product.create({ name, img, price, desc })
+        res.redirect('/products')
+    } catch (err) {
+        res.status(500).render('error', { err: e.message });
+    }
 })
 
 // to show a particular product
-router.get('/products/:id' , async(req,res)=>{
-    let {id} = req.params;
+router.get('/products/:id', async (req, res) => {
+    try {
+        let { id } = req.params;
 
-    // jab ek particular product ko show karna hai to uske sath uske reviews bhi show karne hai to hume us product ke reviews ko populate karna padega
-    let foundProduct = await Product.findById(id).populate('reviews');// here we are populating the reviews aray
-    res.render('products/show' , {foundProduct})
+        // jab ek particular product ko show karna hai to uske sath uske reviews bhi show karne hai to hume us product ke reviews ko populate karna padega
+        let foundProduct = await Product.findById(id).populate('reviews');// here we are populating the reviews aray
+        res.render('products/show', { foundProduct })
+    } catch (err) {
+        res.status(500).render('error', { err: e.message });
+    }
 })
 
 // form to edit the product
-router.get('/products/:id/edit' , async(req,res)=>{
-    let {id} = req.params;
-    let foundProduct = await Product.findById(id);
-    res.render('products/edit' , {foundProduct})
+router.get('/products/:id/edit', async (req, res) => {
+    try {
+        let { id } = req.params;
+        let foundProduct = await Product.findById(id);
+        res.render('products/edit', { foundProduct })
+    } catch (err) {
+        res.status(500).render('error', { err: e.message });
+    }
 })
 
 // to actually edit the data in db
-router.patch('/products/:id' , async(req,res)=>{
-    let {id} = req.params;
-    let {name , img , price , desc} = req.body;
-    await Product.findByIdAndUpdate( id , {name , img , price , desc}  )
-    res.redirect(`/products/${id}`);
+router.patch('/products/:id', async (req, res) => {
+    try {
+        let { id } = req.params;
+        let { name, img, price, desc } = req.body;
+        await Product.findByIdAndUpdate(id, { name, img, price, desc })
+        res.redirect(`/products/${id}`);
+    } catch (err) {
+        res.status(500).render('error', { err: e.message });
+    }
 })
 
 
 // to delete a product
-router.delete('/products/:id' , async(req,res)=>{
-    let {id} = req.params;
-    const product = await Product.findById(id);
+router.delete('/products/:id', async (req, res) => {
+    try {
+        let { id } = req.params;
+        const product = await Product.findById(id);
 
-    // to delete reviews associated with the product -- easy way
-    // for(let id of product.reviews){
-    //     await Review.findByIdAndDelete(id);
-    // }
+        // to delete reviews associated with the product -- easy way
+        // for(let id of product.reviews){
+        //     await Review.findByIdAndDelete(id);
+        // }
 
-    await Product.findByIdAndDelete(id);
-    res.redirect('/products');
+        await Product.findByIdAndDelete(id);
+        res.redirect('/products');
+    } catch (err) {
+        res.status(500).render('error', { err: e.message });
+    }
 })
 
-
 module.exports = router;
- 
